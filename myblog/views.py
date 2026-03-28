151ece04d23f21b0e1545df306e41c02b06745e0
JanuarysG24 <janugiraldo@gmailcom>
2026-03-28 14:32:43 -0500

    avance de la clase del sabado

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    template_name = 'myblog/post_list.html'
+    
+    saludo = 'hola'
+    
+    context ={
+        'saludo': saludo
+    }
+    
+    return render(request, template_name, context)
